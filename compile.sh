866d2224e5c6121f268fdd2bb74e64075b5784a7
dlhxr <dlhxr@163.com>
2013-03-27 17:45:09 +0800

    Add pack tool to make it a flashable zip

---
+++ compile.sh
@@ -0,0 +1,19 @@
+export PATH=/home/hoary/codesourcery/bin:$PATH
+export ARCH=arm
+export CROSS_COMPILE=arm-none-eabi-
+make clean
+make distclean
+make omap4_tuna_config
+make -j8 omap4_tuna
+./pack_tool/mkbootimg --kernel u-boot.bin --ramdisk /dev/null -o u-boot.aimg
+cp -r ./pack_tool/META-INF META-INF
+cp ./pack_tool/signapk.jar signapk.jar 
+cp ./pack_tool/testkey.x509.pem testkey.x509.pem
+cp ./pack_tool/testkey.pk8 testkey.pk8
+zip -r I9250_U-Boot_Kernel.zip META-INF u-boot.aimg 
+java -jar signapk.jar testkey.x509.pem testkey.pk8 I9250_U-Boot_Kernel.zip SIGNED_I9250_U-Boot_Kernel.zip
+rm I9250_U-Boot_Kernel.zip
+rm *.jar
+rm *.pk8
+rm *.pem
+rm -r META-INF 
